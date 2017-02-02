82e8c3da5a934d755514919b2ee5e4ffec5ea575
a.korchagin <a.korchagin@growapp.biz>
2017-02-02 17:41:37 +0300

    fixed vector icon

--- bottom-navigation/src/main/java/it/sephiroth/android/library/bottomnavigation/BottomNavigationItem.java
+++ bottom-navigation/src/main/java/it/sephiroth/android/library/bottomnavigation/BottomNavigationItem.java
@@ -2,6 +2,8 @@ package it.sephiroth.android.library.bottomnavigation;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.content.ContextCompat;
 
 /**
@@ -22,7 +24,11 @@ public class BottomNavigationItem {
     }
 
     protected Drawable getIcon(Context context) {
-        return ContextCompat.getDrawable(context, this.iconResource);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ContextCompat.getDrawable(context, iconResource);
+        } else {
+            return VectorDrawableCompat.create(context.getResources(), iconResource, context.getTheme());
+        }
     }
 
     protected String getTitle() {
